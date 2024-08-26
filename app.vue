fd4df80341a7c0fb500cb4b087faa24caf0fe870
allblack <umh0811@naver.com>
2024-08-26 22:46:37 +0900

    feat: test

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
+<script setup lang="ts">
+useHead({
+  title: "fasdlfnlasdfn",
+  meta: [
+    {
+      key: "description",
+      name: "description",
+      content: "fasdlfnlasdfn",
+    },
+    {
+      key: "og:description",
+      name: "og:description",
+      content: "fasdlfnlasdfn",
+    },
+  ],
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <NuxtPage />
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { ref, toRefs, computed } from "vue";
+</script>
+
+<template>
+  <div class="test">작 업 중</div>
+</template>
+
+<style lang="scss" scoped></style>
