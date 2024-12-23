1aef7367eadb6487bb093331314339b81c2655f3
allblack <umh0811@naver.com>
2024-12-23 21:17:44 +0900

    정리중

--- app.vue
+++ app.vue
@@ -1,21 +1,31 @@
 <script setup lang="ts">
-useHead({
-  title: "fasdlfnlasdfn",
-  meta: [
-    {
-      key: "description",
-      name: "description",
-      content: "fasdlfnlasdfn",
-    },
-    {
-      key: "og:description",
-      name: "og:description",
-      content: "fasdlfnlasdfn",
-    },
-  ],
-});
+//
 </script>
 
 <template>
   <NuxtPage />
 </template>
+
+<style lang="css">
+@import "~/assets/palette.css";
+
+html,
+body {
+  background-color: var(--color-gray-050);
+  font-family: Noto Sans KR, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  width: 100%;
+  height: 100%;
+}
+
+#__nuxt {
+  width: 100%;
+  height: 100%;
+}
+
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,41 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const FILE_NAME = fileURLToPath(import.meta.url);
+const DIR_NAME = dirname(FILE_NAME);
+
+function resolvePath(...paths: string[]) {
+  return resolve(DIR_NAME, ...paths);
+}
+
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  appId: "allblack's blog",
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/icon",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/seo",
+    "@vueuse/nuxt",
+    "@nuxt/fonts",
+  ],
   ssr: false,
   devtools: { enabled: true },
+  alias: {
+    "@": resolvePath(__dirname),
+  },
+  compatibilityDate: "2024-04-03",
   nitro: {
     preset: "node-server",
+  },
+  eslint: {
+    config: {
+      stylistic: {
+        quotes: "double",
+        semi: true,
+      },
+    },
+    checker: true,
+  },
+  fonts: {
   },
 });
